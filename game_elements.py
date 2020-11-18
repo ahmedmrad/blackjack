1b7af6db7d49f451a1201fa736d62e986f98cdba
Ahmed Mrad <mrad.ahmed5@gmail.com>
2020-11-18 11:33:28 +0100

    creating basic classes and few tests

---
+++ game_elements.py
@@ -0,0 +1,85 @@
+import random
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = (
+    'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
+    'Jack', 'Queen', 'King', 'Ace'
+)
+values = {
+    'Two': 2,
+    'Three': 3,
+    'Four': 4,
+    'Five': 5,
+    'Six': 6,
+    'Seven': 7,
+    'Eight': 8,
+    'Nine': 9,
+    'Ten': 10,
+    'Jack': 10,
+    'Queen': 10,
+    'King': 10,
+    'Ace': 1
+}
+
+
+class Card:
+    """docstring for Card"""
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+
+    def __str__(self):
+        return self.rank + ' of ' + self.suit
+
+    def get_suit(self):
+        return self.suit
+
+    def get_rank(self):
+        return self.rank
+
+
+class Deck:
+    """docstring for Deck"""
+
+    def __init__(self):
+        self.all_cards = []
+        for suit in suits:
+            for rank in ranks:
+                self.all_cards.append(Card(suit, rank))
+
+    def __str__(self):
+        c = '''
+    **************************
+    ***** Cards in decks *****
+    **************************
+
+
+    '''
+        for card in self.all_cards:
+            c = c + '      ' + str(card) + '\n'
+        return c
+
+    def __len__(self):
+        return len(self.all_cards)
+
+    def shuffle(self):
+        random.shuffle(self.all_cards)
+
+    def deal_card(self):
+        return self.all_cards.pop()
+
+
+class Player():
+    """docstring for Player"""
+
+    def __init__(self, arg):
+        self.arg = arg
+
+
+class hand():
+    """docstring for hand"""
+
+    def __init__(self, arg):
+        self.arg = arg
---
+++ tests/test_game_elements.py
@@ -0,0 +1,10 @@
+import unittest
+import game_elements
+
+
+class TestGameElements(unittest.TestCase):
+    """docstring for TestGameElements"""
+
+    def __init__(self, arg):
+        super(TestGameElements, self).__init__()
+        self.arg = arg
